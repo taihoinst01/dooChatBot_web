04a1e128eb4ca492511aba575b13d1fc9a9a5751
taihoinst01 <gjlim@taihoinst.com>
2018-02-07 15:01:04 +0900

    Add files via upload

--- webapps/ROOT/assets/js/ui.js
+++ webapps/ROOT/assets/js/ui.js
@@ -1,7 +1,7 @@
 $(function () {
     $('#wrapper').css({ 'height': ($(document).height()) + 'px' });
     $(window).resize(function () {
-        $('#wrapper').css({ 'height': ($(document).height()) + 'px' });
+        //$('#wrapper').css({ 'height': ($(document).height()) + 'px' });
     });
 
     //챗봇창 상단
@@ -9,9 +9,11 @@ $(function () {
         "<span class='chatTitle'></span>" +
         "<span class='chatTitleText'><strong>TaihoBot</strong></span>" +
         "<span class='chatTitleUser'>taiho님과 대화</span>" +
-        "<button class='btnMin'></button>" +
-        "<button class='btnLayer btnLayerFull'></button>" +
-        "<button class='btnClose'></button>" +
+
+        "<span class='topIcon btnClose'><button class='topIcon03'></button></span>" +
+        "<span class='topIcon btnLayer btnLayerFull'><button class='topIcon02'></button></span>" +
+        "<span class='topIcon btnMin'><button class='topIcon01'></button></span>" +
+
         "<div class='chatStatus'>" +
             "<div class='chatStatus1'><strong>TaihoBot</strong><span class='question'></span></div>" +
             "<div class='chatStatus2'>상태 알수 없음</div>" +
@@ -27,4 +29,33 @@ $(function () {
             "<div class='chatFooterIcon5'></div>" +
         "</div > "
     ).appendTo(".wc-chatview-panel");
+
+    //챗봇창 버튼
+    $('.btnClose').click(function () {
+        $('.wc-chatview-panel').css('bottom', 0).hide();
+        $('.bot-wrap').hide().removeClass("chatOn").addClass("chatOff");
+    });
+    $('.btnMin').click(function () {
+        $('.wc-chatview-panel').css({ "overflow": "hidden" })
+        $('.wc-chatview-panel').animate({ "height": "32px" }, "fast");
+        $('.wc-console, wc-message-pane').hide();
+        $('.btnMin').css({ 'display': 'none' });
+        $('.btnLayer').removeClass('btnLayerFull').addClass('btnLayerMid');
+        $('.btnLayer > button').removeClass('topIcon02').addClass('topIcon02-1');
+    });
+    $(document).on('click', '.wc-header [class*=btnLayer]', function () {
+        if ($(this).hasClass('btnLayerMid')) {
+            $('.wc-chatview-panel').css({ "overflow": "visible" })
+            $('.wc-chatview-panel').animate({ "height": "867px" }, "fast");
+            $('.wc-console, wc-message-pane').show();
+            $('.btnLayer').removeClass('btnLayerMid').addClass('btnLayerFull');
+            $('.btnLayer > button').css({ 'display': 'inline-block' }).removeClass('topIcon02-1').addClass('topIcon02');
+            $('.btnMin').css({ 'right': '58px', 'display':'inline-block' });
+        } else {
+            $('.wc-chatview-panel').animate({ "height": ($(document).height()) + 'px' }, "fast");
+            $('.btnLayer').removeClass('btnLayerFull').addClass('btnLayerMid');
+            $('.btnLayer > button').css({ 'display': 'inline-block' }).removeClass('topIcon02').addClass('topIcon02-1');
+        }
+    });
+
 });
